b982c55a90f2373957665455a91be0725e7a0c6a
Alain Wafflard <alain.wafflard@gmail.com>
2024-03-11 21:08:36 +0100

    DB - cours 2024/03/11

---
+++ asset/config/model.php
@@ -0,0 +1,8 @@
+<?php
+
+const DATABASE_TYPE = "MySql";  // "csv"
+const DATABASE_NAME = "4ipdw_2023";
+const DSN =  "mysql:host=localhost;dbname=4ipdw_2023;port=3307";
+const DATABASE_USERNAME = "root";
+const DATABASE_PASSWORD = "";
+
